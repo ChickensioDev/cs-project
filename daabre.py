edbfdccf809d6e3757c211f1be36a45b9f4209a6
ChickensioDev <vireshnambi@gmail.com>
2024-10-05 16:44:13 +0530

    BUTTONN

--- daabre.py
+++ daabre.py
@@ -1,15 +1,21 @@
 
 
 import tkinter as tk
+def _login():
+	win2= tk.Tk()
+	win2.geometry('500x500')
+	b2= tk.Button(win2,text='New window',bg='Red',fg='White')
+	b2.pack()
+	win2.mainloop
+	
+
 win1=tk.Tk()
 win1.geometry("1600x900")
 label = tk.Label(win1, text= "vanakkam da maapla", font = ('Arial', 18))
 label.pack()
-b1= tk.Button(win1,text='Hello world',bg='green')
+b1= tk.Button(win1,text='Hello world',bg='green',  command = "_login")
 b1.pack()
-win2= tk.Tk()
-win2.geometry('500x500')
-b2= tk.Button(win2,text='New window',bg='Red',fg='White')
-b2.pack()
+_login()
+
 win1.mainloop()
-win2.mainloop()
+
