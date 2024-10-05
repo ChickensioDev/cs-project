3882fd2ee66fab0700da3c75722439f2db8e9741
ChickensioDev <vireshnambi@gmail.com>
2024-10-05 16:16:24 +0530

    SEMMMAA

--- daabre.py
+++ daabre.py
@@ -1,7 +1,7 @@
 
-import tkinter
-win1=tkinter.Tk()
-print('hey wow')
-
-print("hi")
-print("bye")
+import tkinter as tk
+win1=tk.Tk()
+win1.geometry("1600x900")
+label = tk.Label(win1, text= "vanakkam da maapla", font = ('Arial', 18))
+label.pack()
+win1.mainloop()
