7094633fc7b7bf6455eda27577f4ba26cf22b832
ChickensioDev <vireshnambi@gmail.com>
2024-10-05 16:54:27 +0530

    final

--- daabre.py
+++ daabre.py
@@ -8,19 +8,20 @@ def _login():
 	b2.pack()
 	win2.mainloop
 	
+def _welcome():
+	win1=tk.Tk()
+	win1.geometry("1600x900")
+	label = tk.Label(win1, text= "vanakkam da maapla", font = ('Arial', 18))
+	label.pack()
+	b1= tk.Button(win1,text='Hello world',bg='green',  command = _login)
+	b1.pack()
 
-win1=tk.Tk()
-win1.geometry("1600x900")
-label = tk.Label(win1, text= "vanakkam da maapla", font = ('Arial', 18))
-label.pack()
-b1= tk.Button(win1,text='Hello world',bg='green',  command = _login)
-b1.pack()
+	a= tk.StringVar()
+	b= tk.StringVar()
+	cb= tk.Checkbutton(win1,text='male',variable=a)
+	cb1= tk.Checkbutton(win1,text='female',variable=b)
+	cb.pack()
+	cb1.pack()
+	win1.mainloop()
 
-a= tk.StringVar()
-b= tk.StringVar()
-cb= tk.Checkbutton(win1,text='male',variable=a)
-cb1= tk.Checkbutton(win1,text='female',variable=b)
-cb.pack()
-cb1.pack()
-win1.mainloop()
-
+_welcome()
