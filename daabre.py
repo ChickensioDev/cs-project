40752fe14bd06d7704ca5c72387b598abaf41bea
ChickensioDev <vireshnambi@gmail.com>
2024-10-05 16:47:54 +0530

    WORKING BUTTON

--- daabre.py
+++ daabre.py
@@ -13,9 +13,9 @@ win1=tk.Tk()
 win1.geometry("1600x900")
 label = tk.Label(win1, text= "vanakkam da maapla", font = ('Arial', 18))
 label.pack()
-b1= tk.Button(win1,text='Hello world',bg='green',  command = "_login")
+b1= tk.Button(win1,text='Hello world',bg='green',  command = _login)
 b1.pack()
-_login()
+
 
 win1.mainloop()
 
