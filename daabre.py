82557b99dcd31b164ed801bf8e9b211865de9753
Harshika <harshika.balakannan@gmail.com>
2024-10-05 16:27:39 +0530

    button

--- daabre.py
+++ daabre.py
@@ -6,5 +6,6 @@ win1.geometry("1600x900")
 label = tk.Label(win1, text= "vanakkam da maapla", font = ('Arial', 18))
 label.pack()
 b1= tk.Button(win1,text='Hello world',bg='green')
+b1.pack()
 win1.mainloop()
 
