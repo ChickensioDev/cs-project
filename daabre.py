ef73425a184d4d077eeaca7df91273862fcb5596
Harshika <harshika.balakannan@gmail.com>
2024-10-05 16:09:43 +0530

    daabreee

--- daabre.py
+++ daabre.py
@@ -4,6 +4,8 @@ win1=tkinter.Tk()
 print('hey wow')
 
 print("hi")
-a=5 
-b=7
-c=a-b
+<<<<<<< HEAD
+print('cs project = amazing')
+=======
+print("bye")
+>>>>>>> 094cc45c8a0e8765bf35dd7294db18d7eb9e956a
