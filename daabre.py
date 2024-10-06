edd38d390e0d46d4b73d51701eeb82712490ec59
ChickensioDev <vireshnambi@gmail.com>
2024-10-06 23:29:59 +0530

    signup confirm password

--- daabre.py
+++ daabre.py
@@ -3,7 +3,9 @@ import pathlib, os
 import tkinter as tk
 from tkinter import PhotoImage
 
+
 def _login():
+	
 	def close_window():
 		win2.withdraw() # Hides the login window, win2
 		win1.deiconify()  #Shows the hidden main window,win1 again
@@ -37,7 +39,7 @@ def _login():
 	pass_label= tk.Label(win2,text='Password',font=("Times New Roman", 18),width=10)
 
 	email_entry= tk.Entry(win2,textvariable=email_var,font=("Times New Roman", 18),width = 30)
-	pass_entry= tk.Entry(win2,textvariable=pass_var,font=("Times New Roman", 18),width = 30,)
+	pass_entry= tk.Entry(win2,textvariable=pass_var,font=("Times New Roman", 18),width = 30,show='*')
 	submit_button= tk.Button(win2,text='Submit',font=("Times New Roman", 18),activeforeground='Green',command=_submit)
 	close_button = tk.Button(win2, text='Back', font=("Times New Roman", 14), activeforeground='Green', command=close_window,height=1,width=4)
 	show_password_check = tk.Checkbutton(win2, text='Show Password', variable=show_password, onvalue=True, offvalue=False, command=password_seen)
@@ -70,6 +72,7 @@ def _signup():
 	email_var=tk.StringVar()
 	pass_var=tk.StringVar()		
 	gender_var = tk.StringVar(value="o") 
+	show_password = tk.BooleanVar()
 
 	def _submit():
 		if age_var.get() and email_var.get() and pass_var.get() and gender_var.get():
@@ -82,6 +85,11 @@ def _signup():
 		else:
 			enter_value= tk.Label(win4,text='Please enter values for all the fields',font=('arial',15),fg='red',bg='white')
 			enter_value.place(relx=0.5,rely=0.5, anchor="center")
+	def password_seen():
+		if show_password.get():
+			pass_entry.config(show='')
+		else:
+			pass_entry.config(show='*')  # Hide the password with *)
 	
 	age_label=tk.Label(win4,text="Age",font=("Times New Roman",18),width=10)
 	email_label=tk.Label(win4,text='Username',font=("Times New Roman",18),width=10)
@@ -89,21 +97,23 @@ def _signup():
 	
 	age_entry=tk.Entry(win4,textvariable=age_var,font=("Times New Roman",18),width = 20)
 	email_entry=tk.Entry(win4,textvariable=email_var,font=("Times New Roman",18), width = 20)
-	pass_entry=tk.Entry(win4,textvariable=pass_var,font=("Times New Roman",18),width = 20)
+	pass_entry=tk.Entry(win4,textvariable=pass_var,font=("Times New Roman",18),width = 20, show='*')
 	
 	button1=tk.Radiobutton(win4,text='Male',variable=gender_var,font=("Times New Roman",18),activeforeground="Green",value="male",width = 7)
 	button2=tk.Radiobutton(win4,text='Female',variable=gender_var,font=("Times New Roman",18),activeforeground="Green",value="female",width=7)
 	submit_button= tk.Button(win4,text='Submit',font=("Times New Roman",18),activeforeground='white', command=_submit)
 	close_button = tk.Button(win4, text='Back', font=("Times New Roman", 14), bg="white", fg="black", command=close_window)
+	show_password_check = tk.Checkbutton(win4, text='Show Password', variable=show_password, onvalue=True, offvalue=False, command=password_seen)
 	
 	email_label.grid(row=0,column=0)
 	email_entry.grid(row=0,column=1)
-	pass_label.grid(row=1,column=0)
-	pass_entry.grid(row=1,column=1)
-	age_label.grid(row=2,column=0)
-	age_entry.grid(row=2,column=1)
-	button1.grid(row=3,column=0,sticky='W')
-	button2.grid(row=3,column=1,sticky='W')
+	pass_label.grid(row=2,column=0)
+	pass_entry.grid(row=2,column=1)
+	show_password_check.grid(row=3, column=1, sticky='w') 
+	age_label.grid(row=1,column=0)
+	age_entry.grid(row=1,column=1)
+	button1.grid(row=4,column=0,sticky='W')
+	button2.grid(row=4,column=1,sticky='W')
 	submit_button.place(relx=0.5,rely=0.4,anchor = 'center')
 	close_button.place(relx=0.5, rely=0.9, anchor='center')
 	
