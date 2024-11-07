e6c5adbbb04e95fc68ef864d36c59790b3e8bb27
ray200208 <rakshaychn2021@gmail.com>
2024-11-08 04:45:29 +0530

    added room feature to sigma file
    transfer it to a new file

--- sigma.py
+++ sigma.py
@@ -54,3 +54,68 @@ id = data[0]
 user = data[1]
 _message()
 app.mainloop()
+
+'''def room1():
+    win3=CTk()
+    win3.geometry("1600x900")
+    win3.title("Be fake")
+
+    frame=CTkFrame(win3,corner_radius=15,width=600,height=600,fg_color='black')
+    frame.place(relx=0.5,rely=0.5,anchor='center')
+    
+    label1=CTkLabel(win3,text='welcome to befake',font=('Arial',28))
+    label1.place(relx=0.5,rely=0.5,anchor='center')
+
+    button1=CTkButton(win3,text='Create Room',font=("Arial",28),command=create_room)
+    button2=CTkButton(win3,text='Join Room',font=("Arial",28),command=join_room)
+
+    button1.place(relx=0.4,rely=0.6,anchor='center')
+    button2.place(relx=0.6,rely=0.6,anchor='center')
+    win3.mainloop()
+
+def create_room():
+    win4=CTk()
+    win4.geometry=("1600x900")
+    win4.title("Room")
+
+    label_name=CTkLabel(win4,text="Name",font=("Arial",28))
+    label_name.place(relx=0.5,rely=0.5,anchor='center')
+    label_pass=CTkLabel(win4,text="Password",font=("Arial",28))
+    label_pass.place(relx=0.48,rely=0.6,anchor="center")
+
+    name_var=StringVar()
+    pass_var=StringVar()
+
+    name_entry=CTkEntry(win4,textvariable=name_var,font=("Times New Roman",28),width=200)
+    name_entry.place(relx=0.6,rely=0.5,anchor='center')
+    pass_entry=CTkEntry(win4,textvariable=pass_var,font=("Times New Roman",28),width=200)
+    pass_entry.place(relx=0.6,rely=0.6,anchor='center')
+
+    create_button=CTkButton(win4,text='Create Room',font=("Times New Roman",28))
+    create_button.place(relx=0.55,rely=0.7,anchor='center')
+
+    win4.mainloop()
+
+def join_room():
+    win5=CTk()
+    win5.geometry=("1600x900")
+
+    label_name=CTkLabel(win5,text="Name",font=("Arial",28))
+    label_name.place(relx=0.5,rely=0.5,anchor='center')
+    label_pass=CTkLabel(win5,text="Password",font=("Arial",28))
+    label_pass.place(relx=0.48,rely=0.6,anchor="center")
+
+    name_var=StringVar()
+    pass_var=StringVar()
+
+    name_entry=CTkEntry(win5,textvariable=name_var,font=("Times New Roman",28),width=200)
+    name_entry.place(relx=0.6,rely=0.5,anchor='center')
+    pass_entry=CTkEntry(win5,textvariable=pass_var,font=("Times New Roman",28),width=200)
+    pass_entry.place(relx=0.6,rely=0.6,anchor='center')
+
+    join_button=CTkButton(win5,text='Join Room',font=("Times New Roman",28))
+    join_button.place(relx=0.55,rely=0.7,anchor='center')
+
+    win5.mainloop()
+    
+room1()'''
