9c175085fe4342081535646c564fe7ea7d60e15d
medha-mk <medha04097@gmail.com>
2024-11-10 01:21:50 +0530

    new

--- daabre.py
+++ daabre.py
@@ -71,6 +71,15 @@ def _login():
 	pass_var= ctk.StringVar()
 	show_password = ctk.BooleanVar()
 	enter_value = ctk.CTkLabel(win2,text='',font=('arial',15),text_color='red',fg_color='white')
+	img_file_name = "login1.png"
+	current_dir = pathlib.Path(__file__).parent.resolve() # current directory
+	img_path = os.path.join(current_dir, img_file_name)
+
+	#Load the image using Pillow
+	image = Image.open(img_path)
+	background_image =ctk.CTkImage(dark_image=image,size=(500,500))
+	background_label = ctk.CTkLabel(win2, image=background_image)
+	background_label.place(relwidth=1, relheight=1)  # Stretch the image to cover the window
 	
 	def _submit():
 			if email_var.get() and pass_var.get():
@@ -88,25 +97,30 @@ def _login():
 			pass_entry.configure(show='')
 		else:
 			pass_entry.configure(show='*')  # Hide the password with *)
-	login_label = ctk.CTkLabel(win2,text='Login',font=('Times New Roman',28),text_color='black',fg_color='transparent')
-	email_label= ctk.CTkLabel(win2,text='Username',font=("Times New Roman", 18),width=10,text_color='Black',fg_color='transparent')
-	pass_label= ctk.CTkLabel(win2,text='Password',font=("Times New Roman", 18),width=10,text_color='Black',fg_color='transparent')
+	login_label = ctk.CTkLabel(win2,text='Login',font=('Berlin Sans FB Demi',28),text_color='black',fg_color='lightblue')
+	email_label= ctk.CTkLabel(win2,text='Username',font=("Berlin Sans FB Demi", 18),width=10,text_color='Black',fg_color='transparent')
+	pass_label= ctk.CTkLabel(win2,text='Password',font=("Berlin Sans FB Demi", 18),width=10,text_color='Black',fg_color='transparent')
 
-	email_entry= ctk.CTkEntry(win2,textvariable=email_var,font=("Times New Roman", 18),width = 150)
-	pass_entry= ctk.CTkEntry(win2,textvariable=pass_var,font=("Times New Roman", 18),width = 150,show='*')
+	email_entry= ctk.CTkEntry(win2,textvariable=email_var,font=("Berlin Sans FB Demi", 18),width = 150)
+	pass_entry= ctk.CTkEntry(win2,textvariable=pass_var,font=("Berlin Sans FB Demi", 18),width = 150,show='*')
 	
-	submit_button= ctk.CTkButton(win2,text='Submit',font=("Times New Roman", 18),command=_submit,text_color='white',fg_color='green')
-	close_button = ctk.CTkButton(win2, text='Back', font=("Times New Roman",14),text_color='white',fg_color='green', command=close_window)
+	submit_button= ctk.CTkButton(win2,text='Submit',font=("Berlin Sans FB Demi", 18),command=_submit,text_color='blue',fg_color='lightblue')
+	close_button = ctk.CTkButton(win2, text='Back', font=("Berlin Sans FB Demi",14),text_color='blue',fg_color='lightblue', command=close_window)
 	show_password_check = ctk.CTkCheckBox(win2, text='Show Password', variable=show_password, onvalue=True, offvalue=False, command=password_seen,text_color='Black')
 	
-	login_label.grid(row=0,column=0,columnspan=3,sticky='')
-	email_label.grid(row=1,column=0)
-	email_entry.grid(row=1,column=1)
-	pass_label.grid(row=2,column=0)
-	pass_entry.grid(row=2,column=1)
-	show_password_check.grid(row=3, column=1, sticky='w')  # Place the checkbox below the password field
-	submit_button.place(relx=0.5,rely=0.4,anchor="center",)
-	close_button.place(relx=0.5,rely=0.9, anchor = "center")
+	login_label.grid(row=0, column=0, columnspan=2, sticky='ew', pady=10)
+
+	win2.grid_rowconfigure(0, weight=1)  
+	win2.grid_rowconfigure(1, weight=1)  
+	login_label.grid(row=2, column=0,sticky='n')
+	email_label.grid(row=3, column=0, padx=5, pady=5, sticky='e')
+	email_entry.grid(row=3, column=1, padx=5, pady=5, sticky='w')
+	pass_label.grid(row=4, column=0, padx=5, pady=5, sticky='e')
+	pass_entry.grid(row=4, column=1, padx=5, pady=5, sticky='w')
+	show_password_check.grid(row=5, column=1, sticky='w', padx=5, pady=5)
+	submit_button.grid(row=6, column=0, pady=20)
+	close_button.grid(row=6, column=1, pady=10)
+	win2.grid_columnconfigure((0, 1), weight=1)
 
 
 	win2.mainloop()
@@ -127,6 +141,15 @@ def _signup():
 	pass_var=ctk.StringVar()		
 	gender_var = ctk.StringVar(value="o") 
 	show_password = ctk.BooleanVar()
+	img_file_name = "login1.png"
+	current_dir = pathlib.Path(__file__).parent.resolve() # current directory
+	img_path = os.path.join(current_dir, img_file_name)
+
+	#Load the image using Pillow
+	image = Image.open(img_path)
+	background_image =ctk.CTkImage(dark_image=image,size=(500,500))
+	background_label = ctk.CTkLabel(win4, image=background_image)
+	background_label.place(relwidth=1, relheight=1)  # Stretch the image to cover the window
 	
 	def _submit():
 		if age_var.get() and email_var.get() and pass_var.get() and gender_var.get() != 'o':
@@ -141,31 +164,47 @@ def _signup():
 		else:
 			pass_entry.configure(show='*')  # Hide the password with *)
 	
-	age_label=ctk.CTkLabel(win4,text="Age",font=("Times New Roman",18),width=10,text_color='black')
-	email_label=ctk.CTkLabel(win4,text='Username',font=("Times New Roman",18),width=10,text_color='black')
-	pass_label=ctk.CTkLabel(win4,text='Password',font=("Times New Roman",18),width=10,text_color='black')
-	
-	age_entry=ctk.CTkEntry(win4,textvariable=age_var,font=("Times New Roman",18),width = 200)
-	email_entry=ctk.CTkEntry(win4,textvariable=email_var,font=("Times New Roman",18), width = 200)
-	pass_entry=ctk.CTkEntry(win4,textvariable=pass_var,font=("Times New Roman",18),width = 200, show='*')
-	
-	button1=ctk.CTkRadioButton(win4,text='Male',variable=gender_var,font=("Times New Roman",18),fg_color="Green",text_color='black',value="male",width = 7)
-	button2=ctk.CTkRadioButton(win4,text='Female',variable=gender_var,font=("Times New Roman",18),fg_color="Green",text_color='black',value="female",width=7)
-	submit_button= ctk.CTkButton(win4,text='Submit',font=("Times New Roman",18), command=_submit,fg_color='Green')
+	age_label = ctk.CTkLabel(win4, text="Age", font=("Times New Roman", 18), width=10, text_color='black')
+	email_label = ctk.CTkLabel(win4, text='Username', font=("Times New Roman", 18), width=10, text_color='black')
+	pass_label = ctk.CTkLabel(win4, text='Password', font=("Times New Roman", 18), width=10, text_color='black')
+
+	age_entry = ctk.CTkEntry(win4, textvariable=age_var, font=("Times New Roman", 18), width=200)
+	email_entry = ctk.CTkEntry(win4, textvariable=email_var, font=("Times New Roman", 18), width=200)
+	pass_entry = ctk.CTkEntry(win4, textvariable=pass_var, font=("Times New Roman", 18), width=200, show='*')
+
+	button1 = ctk.CTkRadioButton(win4, text='Male', variable=gender_var, font=("Times New Roman", 18), fg_color="Green", text_color='black', value="male", width=7)
+	button2 = ctk.CTkRadioButton(win4, text='Female', variable=gender_var, font=("Times New Roman", 18), fg_color="Green", text_color='black', value="female", width=7)
+	submit_button = ctk.CTkButton(win4, text='Submit', font=("Times New Roman", 18), command=_submit, fg_color='Green')
 	close_button = ctk.CTkButton(win4, text='Back', font=("Times New Roman", 14), text_color="white", fg_color="green", command=close_window)
-	show_password_check = ctk.CTkCheckBox(win4, text='Show Password', variable=show_password, onvalue=True, offvalue=False, command=password_seen,text_color='black')
-	
-	email_label.grid(row=0,column=0)
-	email_entry.grid(row=0,column=1)
-	pass_label.grid(row=2,column=0)
-	pass_entry.grid(row=2,column=1)
-	show_password_check.grid(row=3, column=1, sticky='w') 
-	age_label.grid(row=1,column=0)
-	age_entry.grid(row=1,column=1)
-	button1.grid(row=4,column=0,sticky='W')
-	button2.grid(row=4,column=1,sticky='W')
-	submit_button.place(relx=0.5,rely=0.4,anchor = 'center')
-	close_button.place(relx=0.5, rely=0.9, anchor='center')
+	show_password_check = ctk.CTkCheckBox(win4, text='Show Password', variable=show_password, onvalue=True, offvalue=False, command=password_seen, text_color='black')
+
+	# Grid row configuration to push content down
+	win4.grid_rowconfigure(0, weight=1)  
+	win4.grid_rowconfigure(1, weight=1) 
+	win4.grid_rowconfigure(2, weight=0)  
+	win4.grid_rowconfigure(3, weight=0)  
+	win4.grid_rowconfigure(4, weight=0)  
+	win4.grid_rowconfigure(5, weight=0)  
+	win4.grid_rowconfigure(6, weight=0)  
+	win4.grid_rowconfigure(7, weight=1)  
+
+	email_label.grid(row=2, column=0, padx=5, pady=5, sticky='e')  
+	email_entry.grid(row=2, column=1, padx=5, pady=5, sticky='w')  
+
+	age_label.grid(row=3, column=0, padx=5, pady=5, sticky='e')  
+	age_entry.grid(row=3, column=1, padx=5, pady=5, sticky='w')  
+
+	pass_label.grid(row=4, column=0, padx=5, pady=5, sticky='e')  
+	pass_entry.grid(row=4, column=1, padx=5, pady=5, sticky='w')  
+	show_password_check.grid(row=5, column=1, sticky='w', padx=5, pady=5)
+	button1.grid(row=6, column=0, padx=5, pady=5, sticky='w')  
+	button2.grid(row=6, column=1, padx=5, pady=5, sticky='w')  
+	submit_button.grid(row=6, column=0, columnspan=2, pady=20, sticky='n') 
+	close_button.grid(row=7, column=0, columnspan=2, pady=10, sticky='n') 
+	win4.grid_columnconfigure(0, weight=1)
+	win4.grid_columnconfigure(1, weight=1)
+
+
 	win4.mainloop()
 
 
@@ -174,33 +213,33 @@ def _welcome():
 	global win1
 	win1=ctk.CTk() #Creates a CTk window
 	win1.title("Welcome")
-	img_file_name = "bg.png"
+	img_file_name = "welcome.png"
 	current_dir = pathlib.Path(__file__).parent.resolve() # current directory
 	img_path = os.path.join(current_dir, img_file_name)
 
 	#Load the image using Pillow
 	image = Image.open(img_path)
-	background_image =ctk.CTkImage(dark_image=image,size=(1600,900))
+	background_image =ctk.CTkImage(dark_image=image,size=(1000,900))
 	background_label = ctk.CTkLabel(win1, image=background_image)
 	background_label.place(relwidth=1, relheight=1)  # Stretch the image to cover the window
 
-	frame = ctk.CTkFrame(win1, corner_radius=15,width=600,height=600, fg_color="transparent") 
-	frame.place(relx=0.5, rely=0.5, anchor='center')
+	frame = ctk.CTkFrame(win1, corner_radius=15,width=600,height=400, fg_color="violet") 
+	frame.place(relx=0.5, rely=0.55, anchor='center')
 
-	win1.geometry("1600x900")
-	title_label = ctk.CTkLabel(win1, text= "welcome!", font = ('Arial', 28),bg_color='transparent')
-	title_label.place(relx=0.5,rely=0.2,anchor='center')
+	win1.geometry("1000x900")
+	title_label = ctk.CTkLabel(win1, text= "welcome!", font = ('broadway', 40),bg_color='lightpink')
+	title_label.place(relx=0.5,rely=0.25,anchor='center')
 	
-	label1 = ctk.CTkLabel(win1, text= "Already have an account?", font = ('Arial', 24))
-	label1.place(relx=0.5,rely=0.3,anchor='center')
+	label1 = ctk.CTkLabel(win1, text= "Already have an account?", font = ('Berlin Sans FB Demi', 28),fg_color='purple',text_color= 'lightpink')
+	label1.place(relx=0.5,rely=0.4,anchor='center')
 	
-	label2 = ctk.CTkLabel(win1, text= "New to the page?", font = ('Arial', 24))
+	label2 = ctk.CTkLabel(win1, text= "New to the page?", font = ('Berlin Sans FB Demi', 28),fg_color='purple',text_color= 'lightpink')
 	label2.place(relx=0.5,rely=0.6,anchor='center')
 	
-	b1= ctk.CTkButton(win1,text='Login',command = _login,fg_color='green', text_color= 'white', width = 7, height=1, font = ("Times New Roman", 30))
-	b1.place(relx=0.5,rely=0.4, anchor='center')
+	b1= ctk.CTkButton(win1,text='Login',command = _login,fg_color='purple', text_color= 'white', width = 7, height=1, font = ("Berlin Sans FB Demi", 23))
+	b1.place(relx=0.5,rely=0.5, anchor='center')
 	
-	b2= ctk.CTkButton(win1, command = _signup,text='Sign up',fg_color='green',  text_color = 'white', width = 7, height=1,  font = ("Times New Roman", 30))
+	b2= ctk.CTkButton(win1, command = _signup,text='Sign up',fg_color='purple',  text_color = 'white', width = 7, height=1,  font = ("Berlin Sans FB Demi", 23))
 	b2.place(relx=0.5,rely=0.7,anchor = 'center')
 	win1.mainloop()
 
